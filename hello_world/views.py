0f0dede282504a06e4635936c9065273ff6674cb
tomwisgithub <tomwisgithub@users.noreply.github.com>
2021-03-06 13:30:12 +0100

    Added files

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,8 +3,8 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Natalia"
-msg = "Hello World!"
+moje_imie = "Tomek W"
+msg = "Dzien dobry"
 
 @app.route('/')
 def index():
